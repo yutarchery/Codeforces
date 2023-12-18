6655d7d96c48f6aafbf852a33a7acf727e36aebf
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2023-12-18 16:16:28 +0900

    Solve 1900A

---
+++ contests/1801-1900/1900/A.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n;
+string s;
+
+int solve() {
+  cin >> n;
+  cin >> s;
+
+  int cnt = 0;
+  for (int i = 0; i < n; i++) {
+    if (s[i] == '.') {
+      cnt++;
+    } else {
+      cnt = 0;
+    }
+
+    if (cnt >= 3) {
+      return 2;
+    }
+  }
+
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    if (s[i] == '.') {
+      ans++;
+    }
+  }
+  return ans;
+}
+
+int main() {
+  int t;
+  cin >> t;
+
+  for (int i = 0; i < t; i++) {
+    cout << solve() << endl;
+  }
+
+  return 0;
+}
