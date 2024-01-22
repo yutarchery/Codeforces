764f1faa8e0dca80ee39853c8b8c01facf19d8c8
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2024-01-22 19:28:37 +0900

    Solve 1922A

---
+++ contests/1901-2000/1922/A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n;
+string s[3];
+
+bool solve() {
+  cin >> n;
+  for (int i = 0; i < 3; i++) {
+    cin >> s[i];
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (s[0][i] != s[2][i] && s[1][i] != s[2][i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; i++) {
+    if (solve()) {
+      YES();
+    } else {
+      NO();
+    }
+  }
+  return 0;
+}
