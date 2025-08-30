fd0b6a3a4a40470e5906faeae134664d60cb090f
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-08-30 06:53:48 +0000

    Solve 2111A

---
+++ contests/2101-2200/2111/A.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int solve() {
+  ll x;
+  cin >> x;
+
+  ll now[3] = {0, 0, 0};
+  int ans = 0;
+  while (now[0] < x) {
+    ans++;
+    now[0] = min(x, now[1] * 2 + 1);
+    sort(now, now + 3);
+  }
+
+  return ans;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    cout << solve() << endl;
+  }
+
+  return 0;
+}
