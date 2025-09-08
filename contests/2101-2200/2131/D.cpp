4738c34959966907b672874c492e7e554f3ce0ed
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-09-08 07:15:20 +0000

    Solve 2131D

---
+++ contests/2101-2200/2131/D.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, u, v;
+vector<int> graph[int(2e5 + 5)];
+bool is_root[int(2e5 + 5)];
+
+int solve() {
+  cin >> n;
+  fill(is_root + 1, is_root + n + 1, false);
+  for (int i = 1; i <= n; i++) {
+    graph[i].resize(0);
+  }
+
+  for (int i = 1; i <= n - 1; i++) {
+    cin >> u >> v;
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+  }
+
+  int root_count = 0;
+  for (int i = 1; i <= n; i++) {
+    if (graph[i].size() == 1) {
+      is_root[i] = true;
+      root_count++;
+    }
+  }
+
+  int ans = 1e9;
+  for (int i = 1; i <= n; i++) {
+    int now = 0;
+    if (is_root[i]) {
+      now++;
+    }
+    for (int next : graph[i]) {
+      if (is_root[next]) {
+        now++;
+      }
+    }
+
+    ans = min(ans, root_count - now);
+  }
+
+  return ans;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    cout << solve() << endl;
+  }
+
+  return 0;
+}
