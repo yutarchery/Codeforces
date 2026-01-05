7885e3e154b014aeffa6942821c8f2183997fd05
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-01-05 23:49:13 +0900

    Solve 2171A

---
+++ contests/2101-2200/2171/A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int solve() {
+  int n;
+  cin >> n;
+
+  int ans = 0;
+  for (int i = 0; i <= n; i += 2) {
+    if ((n - i) % 4 == 0) {
+      ans++;
+    }
+  }
+
+  return ans;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    cout << solve() << endl;
+  }
+
+  return 0;
+}
