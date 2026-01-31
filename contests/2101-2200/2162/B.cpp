fe955ba1795f15ca48903b1cde03ec64ff254e58
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-01-31 20:41:39 +0900

    Solve 2162B

---
+++ contests/2101-2200/2162/B.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n;
+char s[20];
+
+void solve() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> s[i];
+  }
+
+  vector<int> ans;
+  for (int i = 1; i <= n; i++) {
+    if (s[i] == '1') {
+      ans.push_back(i);
+    }
+  }
+
+  cout << ans.size() << endl;
+  for (int i = 0; i < ans.size(); i++) {
+    cout << ans[i] << (i == ans.size() - 1 ? '\n' : ' ');
+  }
+
+  return;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
