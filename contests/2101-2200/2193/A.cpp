3319caf956956d2a6660ed988edcae0eec08b1a5
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-02-12 06:15:36 +0000

    Solve 2193A

---
+++ contests/2101-2200/2193/A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, s, x, a[15];
+
+bool solve() {
+  cin >> n >> s >> x;
+
+  int sum = 0;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    sum += a[i];
+  }
+
+  if (sum > s) {
+    return false;
+  } else {
+    return (s - sum) % x == 0;
+  }
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    solve() ? YES() : NO();
+  }
+
+  return 0;
+}
