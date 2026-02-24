de7bf4e7f3f38b98676b7c5994a4922c4e1eae7b
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-02-24 22:33:49 +0900

    Solve 977C

---
+++ contests/901-1000/977/C.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, k, a[int(2e5 + 5)];
+
+int main() {
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  sort(a + 1, a + n + 1);
+
+  if (k == 0) {
+    if (a[1] == 1) {
+      cout << -1 << endl;
+    } else {
+      cout << a[1] - 1 << endl;
+    }
+  } else if (a[k] == a[k + 1]) {
+    cout << -1 << endl;
+  } else {
+    cout << a[k] << endl;
+  }
+
+  return 0;
+}
