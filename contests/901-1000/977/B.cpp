ca7c5c9959e20af2b14bb8cb1342a336d52d6a6e
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-02-24 22:27:06 +0900

    Solve 977B

---
+++ contests/901-1000/977/B.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n;
+char s[105];
+
+int cnt[26][26];
+
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> s[i];
+  }
+
+  int cnt_max = 0;
+  for (int i = 1; i + 1 <= n; i++) {
+    cnt[s[i] - 'A'][s[i + 1] - 'A']++;
+
+    if (cnt[s[i] - 'A'][s[i + 1] - 'A'] > cnt_max) {
+      cnt_max = cnt[s[i] - 'A'][s[i + 1] - 'A'];
+    }
+  }
+
+  for (int i = 0; i < 26; i++) {
+    for (int j = 0; j < 26; j++) {
+      if (cnt_max == cnt[i][j]) {
+        cout << char('A' + i) << char('A' + j) << endl;
+        return 0;
+      }
+    }
+  }
+
+  return 0;
+}
