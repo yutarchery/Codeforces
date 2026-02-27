e2c6defe41fa78b995cce05325ab474f931e9722
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-02-27 08:31:42 +0000

    Solve 2126A

---
+++ contests/2101-2200/2126/A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int x;
+    cin >> x;
+
+    vector<int> ans;
+    while (x > 0) {
+      ans.push_back(x % 10);
+      x /= 10;
+    }
+    sort(ans.begin(), ans.end());
+    cout << ans[0] << endl;
+  }
+
+  return 0;
+}
