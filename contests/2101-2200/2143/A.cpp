5b612e2b7beba303c57a8533d1700668023f2007
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-03-10 21:51:53 +0900

    Solve 2143A

---
+++ contests/2101-2200/2143/A.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n, p[105];
+
+void solve() {
+  cin >> n;
+  fill(p, p + n + 2, 0);
+
+  int l, r;
+  for (int i = 1; i <= n; i++) {
+    cin >> p[i];
+
+    if (p[i] == n) {
+      l = i;
+      r = i;
+    }
+  }
+
+  for (int i = n; i >= 1; i--) {
+    for (int j = l; j <= r; j++) {
+      p[j]--;
+    }
+
+    if (p[l - 1] == i - 1) {
+      l--;
+    } else if (p[r + 1] == i - 1) {
+      r++;
+    } else {
+      NO();
+      return;
+    }
+  }
+
+  YES();
+  return;
+}
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
