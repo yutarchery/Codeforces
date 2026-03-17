7a222340dc6fda0998ed94944dd3ea7e79e17c9a
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-03-17 12:03:23 +0900

    Solve 2179B

---
+++ contests/2101-2200/2179/B.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n;
+ll a[int(2e5 + 5)];
+
+ll solve() {
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+
+  ll sum = 0;
+  for (int i = 2; i <= n; i++) {
+    sum += abs(a[i] - a[i - 1]);
+  }
+
+  ll ans = min(sum - abs(a[2] - a[1]), sum - abs(a[n] - a[n - 1]));
+  for (int i = 2; i <= n - 1; i++) {
+    ans = min(ans, sum - abs(a[i] - a[i - 1]) - abs(a[i + 1] - a[i]) +
+                       abs(a[i + 1] - a[i - 1]));
+  }
+
+  return ans;
+}
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    cout << solve() << '\n';
+  }
+
+  return 0;
+}
