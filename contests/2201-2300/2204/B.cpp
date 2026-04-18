3cbaa4ee17a5dff042a4d801c4e82b3449e12578
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-04-18 21:38:52 +0900

    Solve 2204B

---
+++ contests/2201-2300/2204/B.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n, a[int(2e5 + 5)];
+
+int solve() {
+  cin >> n;
+  priority_queue<Pii> que;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    que.push({a[i], i});
+  }
+
+  int ans = 0, r = n + 1;
+  while (!que.empty()) {
+    Pii q = que.top();
+    que.pop();
+
+    if (q.second < r) {
+      ans++;
+      r = q.second;
+    } else {
+      continue;
+    }
+  }
+
+  return ans;
+}
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    cout << solve() << '\n';
+  }
+
+  return 0;
+}
