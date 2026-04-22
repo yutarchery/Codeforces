e3620c9237a58929719822d6b3b3813330a5da89
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-04-22 17:45:30 +0900

    Solve 122B

---
+++ contests/101-200/122/B.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  string s;
+  cin >> s;
+
+  int cnt4 = 0, cnt7 = 0;
+  for (int i = 0; i < s.length(); i++) {
+    if (s[i] == '4') {
+      cnt4++;
+    } else if (s[i] == '7') {
+      cnt7++;
+    }
+  }
+
+  if (cnt4 == 0 && cnt7 == 0) {
+    cout << -1 << '\n';
+  } else if (cnt4 >= cnt7) {
+    cout << 4 << '\n';
+  } else {
+    cout << 7 << '\n';
+  }
+
+  return 0;
+}
