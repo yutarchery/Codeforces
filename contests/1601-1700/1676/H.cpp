a8be3b6603e805fae94e58795a0d93dbc564a687
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-04-24 18:45:53 +0900

    Solve 1676H

---
+++ contests/1601-1700/1676/H.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+struct SegmentTree {
+  int n;
+  vector<ll> node;
+
+  SegmentTree(int sz) {
+    n = 1;
+    while (n < sz) {
+      n *= 2;
+    }
+    node.resize(2 * n - 1, 0);
+  }
+
+  void add(int idx, ll val) {
+    idx += n - 1;
+    node[idx] += val;
+
+    while (idx > 0) {
+      idx = (idx - 1) / 2;
+      node[idx] = node[2 * idx + 1] + node[2 * idx + 2];
+    }
+    return;
+  }
+
+  ll get_sum(int a, int b, int k = 0, int l = 0, int r = -1) {
+    if (r == -1) {
+      r = n;
+    }
+    if (b <= l || r <= a) {
+      return 0;
+    }
+    if (a <= l && r <= b) {
+      return node[k];
+    }
+
+    return get_sum(a, b, 2 * k + 1, l, (l + r) / 2) +
+           get_sum(a, b, 2 * k + 2, (l + r) / 2, r);
+  }
+};
+
+int n, a[int(2e5 + 5)];
+Pii p[int(2e5 + 5)];
+
+ll solve() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    p[i] = {i, a[i]};
+  }
+  sort(p + 1, p + n + 1);
+
+  SegmentTree tree(n + 5);
+
+  ll ans = 0;
+  for (int i = 1; i <= n; i++) {
+    ans += tree.get_sum(p[i].second, n + 1);
+    tree.add(p[i].second, 1);
+  }
+
+  return ans;
+}
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    cout << solve() << '\n';
+  }
+
+  return 0;
+}
