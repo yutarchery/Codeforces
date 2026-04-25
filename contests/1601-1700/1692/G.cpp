19e2ae3525ed219ce87df8980e7cfaf76d9309a4
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-04-25 20:15:08 +0900

    Solve 1692G

---
+++ contests/1601-1700/1692/G.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n, k;
+ll a[int(2e5 + 5)];
+
+int solve() {
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+
+  int ans = 0, cnt = 1;
+  for (int i = 2; i <= n; i++) {
+    if (a[i - 1] < 2 * a[i]) {
+      cnt++;
+    } else {
+      ans += max(0, cnt - k);
+      cnt = 1;
+    }
+  }
+  ans += max(0, cnt - k);
+
+  return ans;
+}
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    cout << solve() << '\n';
+  }
+
+  return 0;
+}
