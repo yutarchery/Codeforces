5dc669fa32420884b4308f84fcb462483f4dab2a
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-04-27 23:53:07 +0000

    Solve 1015A

---
+++ contests/1001-1100/1015/A.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n, m, l[105], r[105];
+int cnt[105];
+vector<int> ans;
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) {
+    cin >> l[i] >> r[i];
+    cnt[l[i]]++;
+    cnt[r[i] + 1]--;
+  }
+
+  for (int i = 1; i <= m; i++) {
+    cnt[i] += cnt[i - 1];
+
+    if (cnt[i] == 0) {
+      ans.emplace_back(i);
+    }
+  }
+
+  cout << ans.size() << '\n';
+  for (int i = 0; i < ans.size(); i++) {
+    cout << ans[i] << (i == ans.size() - 1 ? '\n' : ' ');
+  }
+
+  return 0;
+}
